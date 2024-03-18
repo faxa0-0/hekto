3e60d19c9504f8397972c6a0d9e92f5cc3ae47dd
Faxa2209 <faxa2209@gmail.com>
2024-03-18 16:23:16 +0500

    refactor: added Swiper via node_modules

---
+++ src/sections/hero-slider.js
@@ -0,0 +1,35 @@
+import { onDocumentReady } from '../utils/dom.js';
+import Swiper from 'swiper';
+import { Pagination } from 'swiper/modules';
+
+const state = {
+  elements: {},
+};
+
+const cacheState = () => {
+  state.elements = {
+    sliders: document.querySelectorAll('[data-component="hero-slider"]'),
+  };
+};
+
+// TODO: Pagination doesn't work with multiple instances of swiper 
+
+const initSwipers = () => {
+  state.elements.sliders.forEach((slider) => {
+    const id = slider.id;
+    const swiper = new Swiper(slider, {
+      loop: true,
+      modules: [Pagination],
+      pagination: {
+        el: `.swiper-pagination--${id}`,
+      },
+    });
+  });
+};
+
+const init = () => {
+  cacheState();
+  initSwipers();
+};
+
+onDocumentReady(() => init());
