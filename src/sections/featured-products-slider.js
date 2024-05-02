a295da1e0cd865698f93fd95d7d5822fb5f52d46
Faxa2209 <faxa2209@gmail.com>
2024-05-02 10:48:45 +0500

    feat: featured-products slider added

---
+++ src/sections/featured-products-slider.js
@@ -0,0 +1,86 @@
+import './styles/featured-products-slider.scss';
+import { onDocumentReady } from '../utils/dom.js';
+import Swiper from 'swiper';
+import { Pagination } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import { loadedScripts } from '../utils/loadedScripts.js';
+
+const state = {
+  elements: {},
+};
+
+const addToCart = async (event) => {
+  event.preventDefault();
+  const target = event.target.closest("[data-component='add-to-cart']");
+  try {
+    target.classList.add('active');
+
+    let formData = {
+      items: [
+        {
+          id: target.getAttribute('data-id'),
+          quantity: 1,
+        },
+      ],
+    };
+
+    const res = await fetch(window.Shopify.routes.root + 'cart/add.js', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(formData),
+    });
+    await res.json();
+  } catch (error) {
+    console.error('Error:', error);
+  } finally {
+    target.querySelector('.loader').classList.add('loaded');
+    setTimeout(() => {
+      target.querySelector('.loader').classList.remove('loaded');
+      target.classList.remove('active');
+    }, 2000);
+    state.elements.cart.classList.add('c-cart-drawer--active');
+  }
+};
+
+class AddToCart extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    this.addEventListener('click', (e) => addToCart(e));
+  }
+}
+
+customElements.define('add-to-cart', AddToCart);
+
+const cacheState = () => {
+  state.elements = {
+    sliders: document.querySelectorAll('.featured-products__slider'),
+  };
+};
+
+const initSwipers = () => {
+  state.elements.sliders.forEach((slider) => {
+    const swiper = new Swiper(slider, {
+      slidesPerView: 4,
+      spaceBetween: 29,
+      modules: [Pagination],
+      pagination: {
+        el: slider.querySelector('.featured-products__slider--pagination'),
+        clickable: true,
+      },
+    });
+  });
+};
+
+const init = () => {
+  if (loadedScripts('featured-products')) return;
+  cacheState();
+  initSwipers();
+};
+
+onDocumentReady(() => init());
