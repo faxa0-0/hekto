8d11dfcff6ff48a825a5d4952e746ae70a5be16e
Faxa2209 <faxa2209@gmail.com>
2024-05-02 16:09:09 +0500

    refactor: JS best-practice structure used

--- src/sections/plp-grid.js
+++ src/sections/plp-grid.js
@@ -3,6 +3,10 @@ import { onDocumentReady } from '../utils/dom.js';
 
 const state = {
   elements: {},
+  defaultPage: 2,
+  url: new URL(window.location.href),
+  soloParams: ['sort_by', 'filter.v.price.gte', 'filter.v.price.lte', 'page'],
+  queryParams: new URLSearchParams(window.location.search),
 };
 
 const cacheState = () => {
@@ -11,82 +15,113 @@ const cacheState = () => {
     mobileCloseBtn: document.querySelector('.plp-grid--filters .icon-close'),
     mobileOpenBtn: document.querySelector('.plp-filters--mobile-btn'),
     mobileFilters: document.querySelector('.plp-grid--filters'),
-    sortBy: document.getElementById('sort'),
-    url: new URL(window.location.href),
     collectionGridWrapper: document.getElementById('plp-grid'),
     collectionGrid: document.getElementById('plp-grid-inner'),
-    cart: document.querySelector('.c-cart-drawer'),
-    filterCheckbox: document.querySelectorAll(
-      ".filter-container input[type='checkbox']",
-    ),
-    filterPrice: document.querySelectorAll(
-      ".filter-group-display__price-range input[type='number']",
-    ),
+
+    sortBy: document.getElementById('sort'),
+    filtersContainer: document.querySelector('.filters-form'),
     loadMoreBtn: document.querySelector('.plp-grid--load-more'),
-    defaultPage: 2,
   };
 };
 
-const toggleFilterSidebar = () => {
-  document.body.classList.toggle('overflow-hidden');
+const toggleFilterSidebar = (force) => {
+  document.body.classList.toggle('overflow-hidden', force);
   state.elements.plpOverlay.classList.toggle('active');
   state.elements.mobileFilters.classList.toggle('open');
 };
 
-const resultData = (data) => {
+const renderData = (data) => {
   const newNextPageState = new DOMParser()
     .parseFromString(data, 'text/html')
     .getElementById('plp-grid')
     .getAttribute('data-has-next-page');
+
   const newInnerHtml = new DOMParser()
     .parseFromString(data, 'text/html')
     .getElementById('plp-grid-inner').innerHTML;
+
   state.elements.collectionGridWrapper.setAttribute(
     'data-has-next-page',
     newNextPageState,
   );
+
   state.elements.collectionGrid.innerHTML = newInnerHtml;
 };
 
-function updateUrlParams() {
-  const sortBy = state.elements.sortBy;
-  const params = new URLSearchParams();
-
-  if (sortBy.value) {
-    params.set(sortBy.name, sortBy.value);
-  }
-
-  state.elements.filterCheckbox.forEach((checkbox) => {
-    if (checkbox.checked) {
-      const name = checkbox.getAttribute('name');
-      const value = checkbox.getAttribute('value');
-      params.append(name, value);
-    }
-  });
-
-  state.elements.filterPrice.forEach((price) => {
-    const name = price.getAttribute('name');
-    params.set(name, price.value);
-  });
-
-  state.elements.defaultPage = 2;
-  state.elements.url.search = params.toString();
-  window.history.pushState({}, '', state.elements.url);
-}
-
 const fetchData = async () => {
-  const res = await fetch(state.elements.url);
-  const data = await res.text();
+  try {
+    const res = await fetch(state.url);
+    const data = await res.text();
 
-  if (data) {
-    resultData(data);
+    if (data) {
+      return data;
+    }
+  } catch (error) {
+    console.error(error);
   }
 };
 
-const sortFunction = async (e) => {
-  updateUrlParams();
+function updateUrl() {
+  state.url.search = state.queryParams.toString();
+  window.history.pushState({}, '', state.url);
+}
 
-  fetchData();
+function setUrlParams(name, value) {
+  if (state.queryParams.has(name, value)) {
+    state.queryParams.delete(name, value);
+  } else if (
+    (name == 'filter.v.price.gte' || name == 'filter.v.price.lte') &&
+    value == ''
+  ) {
+    state.queryParams.delete(name);
+  } else if (state.soloParams.includes(name)) {
+    state.queryParams.set(name, value);
+  } else {
+    state.queryParams.append(name, value);
+  }
+}
+
+const onSortAndFilterChange = async (e) => {
+  const name = e.target.attributes.name.value;
+  const value = e.target.value;
+
+  setUrlParams(name, value);
+
+  updateUrl();
+
+  const data = await fetchData();
+
+  renderData(data);
+
+  state.defaultPage = 2;
+};
+
+const loadMoreFunction = async () => {
+  try {
+    const response = await fetch(
+      window.location.pathname +
+        `?section=${window.SectionID}&${state.queryParams.toString()}&page=${state.defaultPage}`,
+    );
+    const html = await response.text();
+    const collectionsWrapper = new DOMParser().parseFromString(
+      html,
+      'text/html',
+    );
+    const items = collectionsWrapper.querySelectorAll('.plp-item');
+    const nextPageState = collectionsWrapper
+      .getElementById('plp-grid')
+      .getAttribute('data-has-next-page');
+    state.elements.collectionGridWrapper.setAttribute(
+      'data-has-next-page',
+      nextPageState,
+    );
+    state.defaultPage += 1;
+    items.forEach((item) => {
+      state.elements.collectionGrid.insertAdjacentElement('beforeend', item);
+    });
+  } catch (error) {
+    console.error('Error loading more:', error);
+  }
 };
 
 const addToCart = async (event) => {
@@ -120,58 +155,23 @@ const addToCart = async (event) => {
       target.querySelector('.loader').classList.remove('loaded');
       target.classList.remove('active');
     }, 2000);
-    state.elements.cart.classList.add('c-cart-drawer--active');
-  }
-};
-
-const loadMoreFunction = async () => {
-  try {
-    const response = await fetch(
-      window.location.pathname +
-        `?section=${window.SectionID}&${window.location.search}&page=${state.elements.defaultPage}`,
-    );
-    const html = await response.text();
-    const collectionsWrapper = new DOMParser().parseFromString(
-      html,
-      'text/html',
-    );
-    const items = collectionsWrapper.querySelectorAll('.plp-item');
-    const nextPageState = collectionsWrapper
-      .getElementById('plp-grid')
-      .getAttribute('data-has-next-page');
-    state.elements.collectionGridWrapper.setAttribute(
-      'data-has-next-page',
-      nextPageState,
-    );
-    state.elements.defaultPage += 1;
-    items.forEach((item) => {
-      state.elements.collectionGrid.insertAdjacentElement('beforeend', item);
-    });
-  } catch (error) {
-    console.error('Error loading more:', error);
   }
 };
 
 const attachEventListeners = () => {
-  state.elements.sortBy.addEventListener('change', sortFunction);
-
-  state.elements.filterPrice.forEach((price) => {
-    price.addEventListener('change', async () => {
-      updateUrlParams();
-      fetchData();
-    });
+  state.elements.sortBy.addEventListener('change', (e) => {
+    onSortAndFilterChange(e);
   });
 
-  state.elements.filterCheckbox.forEach((checkbox) => {
-    checkbox.addEventListener('change', async () => {
-      updateUrlParams();
-      fetchData();
-    });
+  state.elements.filtersContainer.addEventListener('change', async (e) => {
+    onSortAndFilterChange(e);
   });
 
   state.elements.mobileCloseBtn.addEventListener('click', toggleFilterSidebar);
   state.elements.mobileOpenBtn.addEventListener('click', toggleFilterSidebar);
-  state.elements.plpOverlay.addEventListener('click', toggleFilterSidebar);
+  state.elements.plpOverlay.addEventListener('click', () =>
+    toggleFilterSidebar(false),
+  );
 
   state.elements.loadMoreBtn.addEventListener('click', loadMoreFunction);
 };
