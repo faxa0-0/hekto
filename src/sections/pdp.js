8fdf336cf3420a3326c1ae55392037123d715fcb
Faxa2209 <faxa2209@gmail.com>
2024-05-14 08:13:19 +0500

    feat:pdp addToButton functionality added

--- src/sections/pdp.js
+++ src/sections/pdp.js
@@ -8,13 +8,28 @@ import { loadedScripts } from '../utils/loadedScripts.js';
 const state = {
   elements: {},
   lastScrollTop: 0,
+  selectedProductOptionsList: {},
 };
 
 const cacheState = () => {
   state.elements = {
     sliders: document.querySelectorAll('.pdp-grid--container'),
     stickyElement: document.querySelector('[data-sticky-element]'),
+    quantityInput: document.querySelector('#quantity-selector--value'),
+    quantityValueIncrease: document.querySelector(
+      '.quantity-selector--increase',
+    ),
+    quantityValueDecrease: document.querySelector(
+      '.quantity-selector--decrease',
+    ),
+    addToCartButton: document.querySelector('#addToCart-btn'),
+    optionContainer: document.querySelector(
+      '.pdp-info--variant-option--container',
+    ),
   };
+  window.pdpCurrentProduct.options.forEach((option) => {
+    state.selectedProductOptionsList[option] = '';
+  });
 };
 function updateStickyPosition() {
   const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
@@ -26,6 +41,104 @@ function updateStickyPosition() {
   }
 
   state.lastScrollTop = scrollTop <= 0 ? 0 : scrollTop;
+}
+function onQuantityValueIncrease(event) {
+  const currentValue = Number(state.elements.quantityInput.value);
+  state.elements.quantityInput.value = currentValue + 1;
+}
+function onQuantityValueDecrease(event) {
+  const currentValue = Number(state.elements.quantityInput.value);
+  if (currentValue > 1) state.elements.quantityInput.value = currentValue - 1;
+}
+
+function onOptionChange(event) {
+  const name = event.target.attributes.name.value;
+  const value = event.target.value;
+
+  state.selectedProductOptionsList[name] = value;
+
+  const optionTitle = document.getElementById(
+    `pdp-info--variant-${name}-option-title`,
+  );
+  optionTitle.classList.remove('failed');
+  optionTitle.innerHTML = name + ': <span>' + value + '</span>';
+}
+
+function validateData() {
+  const emptyKeys = [];
+
+  for (const key in state.selectedProductOptionsList) {
+    if (
+      state.selectedProductOptionsList.hasOwnProperty(key) &&
+      state.selectedProductOptionsList[key] === ''
+    ) {
+      emptyKeys.push(key);
+    }
+  }
+  if (emptyKeys.length != 0) {
+    return showError(emptyKeys);
+  }
+
+  const selectedVariant = window.pdpCurrentProduct.variants.find(
+    (variant) =>
+      variant.title ===
+      Object.values(state.selectedProductOptionsList).join(' / '),
+  );
+
+  if (!selectedVariant) {
+    return showError("It's just impossible");
+  }
+  return selectedVariant.id;
+}
+function showError(value) {
+  switch (typeof value) {
+    case 'string':
+      state.elements.addToCartButton.textContent = 'Failed to Add';
+      state.elements.addToCartButton.classList.add('failed');
+      console.error('there is no ID in variant');
+      break;
+    case 'object':
+      if (Array.isArray(value)) {
+        value.map((option) => {
+          let optionTitle = document.getElementById(
+            `pdp-info--variant-${option}-option-title`,
+          );
+          optionTitle.textContent = `Please select your ${option}`;
+          optionTitle.classList.add('failed');
+        });
+      }
+      break;
+    default:
+  }
+}
+
+function addToCart() {
+  const selectedProductID = validateData();
+
+  if (!selectedProductID) return;
+
+  const selectedProductData = {
+    items: [
+      {
+        id: selectedProductID,
+        quantity: state.elements.quantityInput.value,
+      },
+    ],
+  };
+
+  fetch(window.Shopify.routes.root + 'cart/add.js', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(selectedProductData),
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    });
 }
 
 const initSwipers = () => {
@@ -66,6 +179,16 @@ const attachEventListeners = () => {
   initSwipers();
 
   window.addEventListener('scroll', updateStickyPosition);
+  state.elements.quantityValueIncrease.addEventListener('click', (e) =>
+    onQuantityValueIncrease(e),
+  );
+  state.elements.quantityValueDecrease.addEventListener('click', (e) =>
+    onQuantityValueDecrease(e),
+  );
+  state.elements.addToCartButton.addEventListener('click', addToCart);
+  state.elements.optionContainer.addEventListener('change', (e) =>
+    onOptionChange(e),
+  );
 };
 
 const init = () => {
@@ -74,4 +197,4 @@ const init = () => {
   attachEventListeners();
 };
 
-onDocumentReady(() => init());
+onDocumentReady(init);
